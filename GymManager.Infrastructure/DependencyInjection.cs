dc3076c39b7caa3619485f54d0fa3ca5341b4452
mmakselon <magdalena.pachacz@gmail.com>
2024-08-10 23:45:00 +0200

    podzial projektow

---
+++ GymManager.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManager.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        //
+        return services;
+    }
+
+    public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+    {
+        //
+        return services;
+    }
+}
