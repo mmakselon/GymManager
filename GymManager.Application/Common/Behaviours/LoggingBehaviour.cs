2eb563393ef8804bf6f109900dcfe79dbc76ed16
mmakselon <magdalena.pachacz@gmail.com>
2025-01-19 23:35:38 +0100

    add LoggingBehaviour

---
+++ GymManager.Application/Common/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace GymManager.Application.Common.Behaviours;
+public class LoggingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger _logger;
+
+    public LoggingBehaviour(ILogger<TRequest> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var requestName = typeof(TRequest).Name;
+
+        _logger.LogInformation($"Handling {requestName}");
+
+        _logger.LogInformation("GymManager Request: {@Name} {@Request}", requestName, request);
+
+        var response = await next();
+
+        _logger.LogInformation($"Handled {typeof(TResponse).Name}");
+
+        return response;
+    }
+
+}
