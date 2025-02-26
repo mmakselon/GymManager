e49ef1346e7b23a31c14982eeeb4e66313669e4b
mmakselon <magdalena.pachacz@gmail.com>
2025-02-26 22:10:10 +0100

    add Contact form

--- GymManager.UI/Controllers/HomeController.cs
+++ GymManager.UI/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using GymManager.Application.Contacts.Commands.SendContactEmail;
 using GymManager.Application.Tickets.Command.AddTicket;
 using GymManager.Application.Tickets.Queries.GetTicketById;
 using GymManager.UI.Models;
@@ -9,14 +10,14 @@ namespace GymManager.UI.Controllers
 {
     public class HomeController : BaseController
     {
-        
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-        
+
 
         public async Task<IActionResult> Index()
         {
@@ -30,10 +31,10 @@ namespace GymManager.UI.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Contact()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new SendContactEmailCommand());
         }
+
     }
 }
